815516ed4dd41cb6c91cf2b155e1d5a226143fdd
NamiDorama <yulliavoitiuk@gmail.com>
2018-04-10 13:48:09 +0300

    added modified map

--- src/GetLocation/GetLocation.js
+++ src/GetLocation/GetLocation.js
@@ -4,10 +4,40 @@ import './getLocation.scss';
 export class GetLocation extends Component {
   state = {
     location: null,
-    error: null
+    error: null,
+    usersLocation: {
+      lat: null,
+      lng: null
+    },
   };
 
   getPosition = () => {
+    const latInput = document.getElementById('lat');
+    const lngInput = document.getElementById('lng');
+    console.log(!!(latInput.value && lngInput.value));
+
+    if (!!(latInput.value && lngInput.value)) {
+      console.log(latInput.value, lngInput.value);
+      const latValue = +latInput.value;
+      const latMin = +latInput.min;
+      const lngMax = +latInput.max;
+      const value = +lngInput.value;
+      const min = +lngInput.min;
+      const max = +lngInput.max;
+
+      console.log(latValue, value);
+
+      if ((latValue >= latMin && latValue <= lngMax) && (value >= min && value <= max)) {
+        console.log('hi');
+        this.setState({ usersLocation: {
+          [latInput.name]: latValue,
+          [lngInput.name]: value
+        }
+        });
+        return;
+      }
+    }
+
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         location => this.setState({ location }),
@@ -30,11 +60,27 @@ export class GetLocation extends Component {
     return (
       <div className="get-location">
         <h2>This is GetLocation</h2>
+        <input
+          id="lat"
+          type="text"
+          placeholder="Enter latitude"
+          min="-90"
+          max="90"
+          name="lat"
+        />
+        <input
+          id="lng"
+          type="text"
+          placeholder="Enter longitude"
+          min="-180"
+          max="180"
+          name="lng"
+        />
         <button onClick={this.getPosition}>
           Get your location
         </button>
         {
-          this.state.location ?
+          this.state.location || this.state.usersLocation.lat && this.state.usersLocation.lng ?
             <React.Fragment>
               <div>
                 <span>Your latitude: {this.state.location.coords.latitude}</span>
@@ -42,8 +88,8 @@ export class GetLocation extends Component {
               </div>
               <MyMapComponent
                 isMarkerShown
-                lat={this.state.location.coords.latitude}
-                lng={this.state.location.coords.longitude}
+                lat={this.state.usersLocation.lat ? this.state.usersLocation.lat : this.state.location.coords.latitude}
+                lng={this.state.usersLocation.lng ? this.state.usersLocation.lng : this.state.location.coords.longitude}
                 googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCj2pn69UXjrrph-__AW_MxWRp04yIhuEY
                 &v=3.exp&libraries=geometry,drawing,places"
                 loadingElement={<div style={{ height: '100%' }} />}
