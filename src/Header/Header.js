5c927c1c3dae7bb02a3c8a890da009184f5367d1
NamiDorama <yulliavoitiuk@gmail.com>
2018-04-17 17:58:14 +0300

    17.04

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,13 +1,11 @@
 import { Navigation } from '../Navigation';
 import './header.scss';
 
-const Heading = () => <h1>Here is the heading!</h1>;
 const navList = ['Home', 'Products', 'Contacts'];
 
 export const Header = () => (
   <div id="header">
-    <img src="./img/jake.gif" alt="Cute cat" />
-    <Heading />
+    <a href="/"><img src="./img/tik.png" alt="Task manager" title="Task manager" /></a>
     <Navigation list={navList} />
   </div>
 );
